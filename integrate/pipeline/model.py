7000e4fdaaf63f0082bd96d4187cfd0af00030f0
Rohith-Yogi <142866048+Rohith-Yogi@users.noreply.github.com>
2024-04-10 21:13:53 +0000

    merged with pipeline

---
+++ integrate/pipeline/model.py
@@ -0,0 +1,21 @@
+from pipeline.nfs.nfs import Model as Nfs
+from pipeline.deepMalwareDetectionCore.src.deepMalwareDetectionFunc.test import deepMalwareDetection
+import numpy as np
+
+
+class Model:
+    def __init__(self):
+        self.models = [
+            Nfs()
+        ]
+
+    def predict_threshold(self, bytez, threshold=0.8) -> int:
+        results = [model.predict_threshold(bytez, threshold) for model in self.models]
+
+        return 1 if np.mean(results) >= threshold else 0
+
+
+# if __name__ == '__main__':
+#     model = Model()
+
+# deepMalwareDetection()
