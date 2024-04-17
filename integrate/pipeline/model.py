cc670f97308c3c0b9b6ac6f49273c5fcb4a02a96
Rohith-Yogi <142866048+Rohith-Yogi@users.noreply.github.com>
2024-04-17 11:05:44 +0000

    added testing

--- integrate/pipeline/model.py
+++ integrate/pipeline/model.py
@@ -5,6 +5,22 @@ from pipeline.deepMalwareDetectionCore.src.deepMalwareDetectionFunc.test import 
 from pipeline.FFNN.model import Model as FFNN
 import numpy as np
 
+# All for 0.6 threshold - on the test data https://drive.google.com/file/d/16n_wlyAnAHL1aIHgnkbbIEjQ0SM2R259/view 
+# Model - gooware% malware%. - check set wise results for goodware and malware (vishal) - pick best one
+# Malcov+, 2xFFNN 98.75% and 90.03%. 
+# only FFNN 97.81% and 92.21% 
+# Malcov+, attention, 3xFFNN 97.81 and 91.59
+# Malcov+, attention, 2xFFNN 91.8 and 89.41
+# MalCov+, Attention and FFNN 85% and 87%
+# (our submitted model ->) 2xMalcov+, FFNN 86.25% and 79.44%
+# All models 83% gw and 83% mw
+# Malcov+ and attention 80 and 80
+# MalCoV+, base and attention 81.88% and 74.7%
+# only Malcov+ 81.88 and 75.70 
+# only attention 77.50 and 86.92
+# NFS, Malcov+, Attention 77% and 85%
+# only Malcov base 86.88% and 60.12
+# only NFS 40% and 87.54%
 
 class Model:
     def __init__(self):
@@ -13,12 +29,14 @@ class Model:
             MalConvPlus_model(),
             MalConvBase_model(),
             AttentionRCNN_model(),
-            FFNN()
+            FFNN(),
         ]
 
     def predict_threshold(self, bytez, threshold=0.5) -> int:
         results = [model.predict_threshold(bytez, threshold) for model in self.models]
-
+        # results.append(results[-1])
+        # results.append(results[-1])
+        # print(results)
         return 1 if np.mean(results) >= threshold else 0
 
 
---
+++ integrate/test-with-data.py
@@ -0,0 +1,85 @@
+import time
+import zipfile
+from typing import Tuple, Union, Dict
+import json
+
+import tqdm
+import requests as rq
+from pipeline.model import Model
+from pipeline.nfs.nfs import NeedForSpeedModel
+
+def stats(results: Dict[str, int]) -> Tuple[float, int]:
+    acc = (sum([1 for value in results.values() if value == 1]) / len(results)) * 100
+    defaults = sum([1 for value in results.values() if value == -1])
+
+    return acc, defaults
+
+
+def pretty_print(results: Dict[str, int], goodware: bool=True) -> None:
+    acc, defaults = stats(results)
+
+    if goodware:
+        acc = 100 - acc
+
+    print(f'{"benign" if goodware else "malware"} | Accuracy: {acc:.2f}% | Defaults: {defaults} | #samples: {len(results)}')
+
+
+if __name__ == '__main__':
+    data_path = 'test-data/z-data.zip'
+    url = "http://127.0.0.1:8080"
+
+    results = {}
+    password = b'infected'
+    sample_size = 1000000000
+    model = Model()
+    threshold = 0.6
+    online = False
+
+    with zipfile.ZipFile(data_path, 'r') as f:
+        with tqdm.tqdm(total=min(len(f.infolist()), sample_size), desc='Processing') as pbar:
+            for info in f.infolist():
+                if (not info.is_dir()) and sample_size > 0:
+
+                    try:
+
+                        if not online:
+                            content = f.read(info.filename, pwd=b'infected')
+                            results[info.filename] = model.predict_threshold(content, threshold)
+                        else:
+                            resp = rq.post(url, headers={'Content-Type': 'application/octet-stream'}, data=content, timeout=5)
+
+                            if resp.status_code == 200:
+                                results[info.filename] = resp.json()['result']
+                            else:
+                                results[info.filename] = 0
+
+                    except Exception as e:
+                        print(e)
+                        results[info.filename] = -1
+
+                    sample_size -= 1
+                    pbar.update(1)
+
+                    if sample_size == 0:
+                        break
+
+    with open('output.json', 'w') as f:
+        json.dump(results, f, indent=4)
+
+    # with open('output.json', 'r') as f:
+    #     results = json.load(f)
+
+    # process results
+    goodware_results = {
+        key: value for key, value in results.items() if '/gw' in key
+    }
+
+    malware_results = {
+        key: value for key, value in results.items() if '/mw' in key
+    }
+
+    g_acc = pretty_print(goodware_results, True)
+    m_acc = pretty_print(malware_results, False)
+
+
+
